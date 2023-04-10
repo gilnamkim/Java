529f12e32bde2594055810e2f1542729ce776194
gilnamkim <dustin0627@naver.com>
2023-04-10 17:42:43 +0900

    정수자료형

---
+++ Chap02/Datatype01.java
@@ -0,0 +1,15 @@
+package Chap02;
+
+public class Datatype01 {
+    public static void main(String[] args) {
+        int a = 10;
+        short s = 2;
+        byte b = 6;
+        long l = 1253621332135L; //int형 범위를 벗어나면 long형 이라는 표시를 해주어야 합니다.
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(l);
+    }
+    
+}
