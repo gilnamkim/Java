bf61c523ba381b45214d9d27eaa35fad478eb8aa
gilnamkim <dustin0627@naver.com>
2023-04-10 16:55:05 +0900

    Variable01추가

---
+++ Chap01/Variable01.java
@@ -0,0 +1,14 @@
+package Chap01;
+
+public class Variable01 {
+    public static void main(String[] args) {
+        int days = 10; //int형 선언과 동시에 값을 설정
+        System.out.println(days);
+
+        int speed; //먼저 int형 선언 후
+        speed = 20; //나중에 값을 설정
+        System.out.println(speed);
+
+    }
+    
+}
