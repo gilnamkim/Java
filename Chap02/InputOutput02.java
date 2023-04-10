8b38ec628b0d2de8879e05fa1aaf68cb3696e268
gilnamkim <dustin0627@naver.com>
2023-04-10 18:13:17 +0900

    이스케이프출력2

---
+++ Chap02/InputOutput02.java
@@ -0,0 +1,11 @@
+package Chap02;
+
+public class InputOutput02 {
+    public static void main(String[] args) {
+        String myName = "홍길동";
+        int myAge = 20;
+
+        System.out.print("이름:\t"+myName+"\n");
+        System.out.printf("나이:\t%d\n",myAge);
+    }
+}
