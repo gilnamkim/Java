f725d551eb6af22ea0ab39aaa74feb9f6b5a278a
gilnamkim <dustin0627@naver.com>
2023-04-10 18:08:39 +0900

    복합출력

---
+++ Chap02/InputOutput01.java
@@ -0,0 +1,11 @@
+package Chap02;
+
+public class InputOutput01 {
+    public static void main(String[] args) {
+        String myName = "홍길동";
+        int myAge = 20;
+
+        System.out.println("이름: " + myName);
+        System.out.printf("나이: %d",myAge);
+    }
+}
