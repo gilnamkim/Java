c594db33964c22057167ccba166ca2436a349845
gilnamkim <dustin0627@naver.com>
2023-04-10 19:41:08 +0900

    변수형을 리터럴뒤에 붙여줘야하는 이유 추가

--- Chap02/Datatype02.java
+++ Chap02/Datatype02.java
@@ -4,6 +4,8 @@ public class Datatype02 {
     public static void main(String[] args) {
         float f = 65.20298f; //float형 4바이트 변수 설정
         double d = 876.765d; //double형 8바이트 변수 설정
+        // long형과 마찬가지로 임시저장되는 int형에 벗어나기 때문에 변수형을 붙여줘야한다.
+        
         System.out.println(f);
         System.out.println(d);
     }
