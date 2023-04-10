364e137163f55add6ecee085f289a6f64ac05e59
gilnamkim <dustin0627@naver.com>
2023-04-10 16:58:44 +0900

    상수선언

---
+++ Chap01/Variable02.java
@@ -0,0 +1,10 @@
+package Chap01;
+
+public class Variable02 {
+    public static void main(String[] args) {
+        final double PI = 3.14; // double형의 PI는 상수입니다
+        System.out.println("반지름 3인 원의 넓이를 구합니다.");
+        System.out.println(3*3*PI);
+    }
+    
+}
