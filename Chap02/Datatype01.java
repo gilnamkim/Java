f811ad7475475ee4a7e37421c3d28cdb33a029b0
gilnamkim <dustin0627@naver.com>
2023-04-10 17:47:33 +0900

    실수자료형

--- Chap02/Datatype01.java
+++ Chap02/Datatype01.java
@@ -5,7 +5,7 @@ public class Datatype01 {
         int a = 10;
         short s = 2;
         byte b = 6;
-        long l = 1253621332135L; //int형 범위를 벗어나면 long형 이라는 표시를 해주어야 합니다.
+        long l = 1253621332135L; //임시저장되는 데이터타입이 int형이라 int형 범위에 벗어나는 값은 변수형을 붙여줘야한다.
         System.out.println(a);
         System.out.println(s);
         System.out.println(b);
