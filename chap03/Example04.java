1885f51b6867e6a44a92bdadce83f582fbfc9314
gilnamkim <dustin0627@naver.com>
2023-04-11 17:53:15 +0900

    주석 추가

--- chap03/Example04.java
+++ chap03/Example04.java
@@ -4,6 +4,11 @@ public class Example04 {
     public static void main(String[] args) {
         int x = 10;
 
+        // < 할당연산자 >
+        // 변수에 값을 할당하는 데 사용함
+        // 왼쪽 피연산자가 변수이고 오른쪽 피연산자가 값
+        //   > 오른쪽에 있는 값은 왼쪽에 있는 피연산자의 자료형과 같아야 함
+        //   > 그렇지 않으면 컴파일러에서 오류가 발생함
         System.out.println(x);
         x += 2;
         System.out.println(x);
