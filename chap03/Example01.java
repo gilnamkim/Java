46c15fb39550f810722a54761e29f169659bba54
gilnamkim <dustin0627@naver.com>
2023-04-11 17:27:36 +0900

    주석 추가

--- chap03/Example01.java
+++ chap03/Example01.java
@@ -4,11 +4,11 @@ public class Example01 {
     public static void main(String[] args) {
         int x = 10, y = 20;
 
-        //연산자 모음입니다
-        System.out.println("x + y = "+(x+y));
-        System.out.println("x - y = "+(x-y));
-        System.out.println("x * y = "+(x*y));
-        System.out.println("x / y = "+(x/y));
-        System.out.println("x % y = "+(x%y));
+        //산술연산자 : 수학 표현식의 덧셈, 뺄셈 등과 같은 산술 연산을 수행하는데 사용합니다.
+        System.out.println("x + y = "+(x+y)); // 왼쪽 피연산자(x)와 오른쪽 피연산자(y)를 더합니다.
+        System.out.println("x - y = "+(x-y)); // 왼쪽 피연산자(x)에서 오른쪽 피연산자(y)를 뺍니다.
+        System.out.println("x * y = "+(x*y)); // 왼쪽 피연산자(x)와 오른쪽 피연산자(y)를 곱합니다.
+        System.out.println("x / y = "+(x/y)); // 왼쪽 피연산자(x)를 오른쪽 피연산자(y)로 나누고 몫을 반환합니다.
+        System.out.println("x % y = "+(x%y)); // 왼쪽 피연산자(x)를 오른쪽 피연산자(y)로 나누고 나머지 값을 반환합니다.
     }
 }
