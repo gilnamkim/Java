b1e2d9866dbe4b7c7e095876aba3c13f3c1ccdc5
gilnamkim <dustin0627@naver.com>
2023-04-11 17:31:32 +0900

    주석 추가

--- chap03/Example02.java
+++ chap03/Example02.java
@@ -4,7 +4,11 @@ public class Example02 {
     public static void main(String[] args) {
         int x = 10, y = 10;
 
-        System.out.println(x==y); //참이면 true 거짓이면 false를 반환합니다.
+        // < 관계연산자 >
+        // 두 피연산자의 크고 작음을 비교하기 위해 사용합니다.
+        // 두 피연산자의 관계가 참이면 true를 반환하고 거짓이면 false를 반환합니다.
+        // 일반적으로 관계 연산자는 if문, while문 등에서 조건식을 판별하는데 사용합니다. 
+        System.out.println(x==y); 
         System.out.println("x와 y는 같습니까? "+(x==y));
     }
 }
--- chap03/Operator02.java
+++ chap03/Operator02.java
@@ -4,11 +4,11 @@ public class Operator02 {
     public static void main(String[] args) {
         int a = 10, b = 25;
         
-        System.out.println("a == b = "+(a==b));
-        System.out.println("a != b = "+(a!=b));
-        System.out.println("a > b = "+(a>b));
-        System.out.println("a < b = "+(a<b));
-        System.out.println("a <= b = "+(a<=b));
-        System.out.println("a >= b = "+(a>=b));
+        System.out.println("a == b = "+(a==b)); // 왼쪽 피연산자(a)와 오른쪽 피연산자(b)가 같으면 true
+        System.out.println("a != b = "+(a!=b)); // 왼쪽 피연산자(a)와 오른쪽 피연산자(b)가 같지 않으면 true
+        System.out.println("a > b = "+(a>b)); // 왼쪽 피연산자(a)가 오른쪽 피연산자(b)보다 크면 true
+        System.out.println("a < b = "+(a<b)); // 왼쪽 피연산자(a)가 오른쪽 피연산자(b)보다 작으면 true
+        System.out.println("a <= b = "+(a<=b)); // 왼쪽 피연산자(a)가 오른쪽 피연산자(b)보다 작거나 같으면 true
+        System.out.println("a >= b = "+(a>=b)); // 왼쪽 피연산자(a)가 오른쪽 피연산자(b)보다 크거나 같으면 true
     }
 }
