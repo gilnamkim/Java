0656a34900531db4c848693ba1ffa042b926cbab
gilnamkim <dustin0627@naver.com>
2023-04-11 10:50:03 +0900

    관계연산자추가

---
+++ chap03/Example02.java
@@ -0,0 +1,10 @@
+package chap03;
+
+public class Example02 {
+    public static void main(String[] args) {
+        int x = 10, y = 10;
+
+        System.out.println(x==y); //참이면 true 거짓이면 false를 반환합니다.
+        System.out.println("x와 y는 같습니까? "+(x==y));
+    }
+}
