5b6eff0d048d1c23d271b7559ddc0835a6f9e7d0
gilnamkim <dustin0627@naver.com>
2023-04-11 17:17:25 +0900

    할당연산자 예제

---
+++ chap03/Operator04.java
@@ -0,0 +1,17 @@
+package chap03;
+
+public class Operator04 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.println(c);
+        System.out.println(c = a);
+        System.out.println(b += a);
+        System.out.println(b -= a);
+        System.out.println(b *= a);
+        System.out.println(b /= a);
+        System.out.println(b %= a);
+    }
+}
