5e1a80193f81e70dfdf54b39de56e6d0fec50cc9
gilnamkim <dustin0627@naver.com>
2023-04-12 15:45:07 +0900

    break문 예제

---
+++ chap04/Break01.java
@@ -0,0 +1,10 @@
+package chap04;
+
+public class Break01 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            if(i == 5) break; // i가 5가 되면 break
+            System.out.println("i: "+ i);
+        }
+    }
+}
