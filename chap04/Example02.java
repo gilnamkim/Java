7ccc6041f5d8398cf6581e2db97dff85fad5bf27
gilnamkim <dustin0627@naver.com>
2023-04-12 14:31:33 +0900

    조건문if_else 예제

---
+++ chap04/Example02.java
@@ -0,0 +1,12 @@
+package chap04;
+
+public class Example02 {
+    public static void main(String[] args) {
+        int num = 120;
+
+        // if ~ else문
+        // if 문과 else 문을 함께 사용하여 조건식의 결과에 따라 특정 코드 또는 코드 블록의 실행을 선택하는 조건문 입니다.
+        if(num < 50) System.out.println("숫자가 50보다 작습니다.");
+        else System.out.println("숫자가 50보다 큽니다.");
+    }
+}
