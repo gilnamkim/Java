3548bb215a16f1dff5b887d7104548c15ce2cdce
gilnamkim <dustin0627@naver.com>
2023-04-12 15:17:48 +0900

    while문 예시

---
+++ chap04/Example08.java
@@ -0,0 +1,16 @@
+package chap04;
+
+public class Example08 {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // while문
+        // 조건식이 참이면 루프를 계속 반복하는 반복문입니다.
+        // 조건식은 대개 비교 또는 논리 연산식입니다.
+        // while 문은 반복횟수가 정해져 있지 않을 때 사용합니다.
+        while(i <= 10){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
