0465fb6fe5c20d9fde926ef61d44cd3ef813465e
gilnamkim <dustin0627@naver.com>
2023-04-12 15:13:24 +0900

    for문 예시

---
+++ chap04/Example07.java
@@ -0,0 +1,12 @@
+package chap04;
+
+public class Example07 {
+    public static void main(String[] args) {
+        int i;
+
+        // for문
+        // 특정 횟수만큼 실행해야 하는 루프를 효율적으로 쓸 수 있는 반복문입니다.
+        // 작업을 반복하는 횟수를 알고 있을 때 유용합니다.
+        for(i = 0; i <= 10; i++) System.out.print(i+" ");
+    }
+}
