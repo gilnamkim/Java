b10a112e6b674cb0b54db0c519c837ea8bbcc5e0
gilnamkim <dustin0627@naver.com>
2023-04-20 15:06:42 +0900

    입력과 출력이 없는 메서드 예시

---
+++ chap05/Example04.java
@@ -0,0 +1,15 @@
+package chap05;
+
+public class Example04 {
+
+    // 반환 유형이 없는 메서드
+    // 입력과 출력이 없는 메서드
+
+    public static void print() {
+        System.out.println("Hi! Java");
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+}
