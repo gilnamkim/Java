3514363a331f67397c3410c0de2cd26475d0af8b
gilnamkim <dustin0627@naver.com>
2023-04-20 18:04:45 +0900

    public 접근제한자를 이용하여 클래스 멤버 호출하기

--- chap07/AccessModifier01.java
+++ chap07/AccessModifier01.java
@@ -1,6 +1,9 @@
 package chap07;
 
 public class AccessModifier01 {
+
+	// public 접근 제한자
+	//	>> public 접근제한자가 선언된 클래스 멤버는 클래스가 같은 패키지에 있든 다른 패키지에 있든 상관없이 어디서나 직접 접근할 수 있음
 	public static void main(String[] args) {
 		Dog dogObj = new Dog();
 		
