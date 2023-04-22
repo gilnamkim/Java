f7b3b50d09417707cd9726a5e5e4c6c130862d9e
gilnamkim <dustin0627@naver.com>
2023-04-22 11:24:54 +0900

    다단계상속 예시

---
+++ chap08/Example03.java
@@ -0,0 +1,10 @@
+package chap08;
+
+public class Example03 {
+    public static void main(String[] args) {
+        Kitten objKitten = new Kitten();
+        objKitten.meow2();
+        objKitten.meow();
+        objKitten.eat();
+    }
+}
