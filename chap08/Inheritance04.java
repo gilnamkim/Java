4de98bba5e37ecc3802950f163cf476c2c333861
gilnamkim <dustin0627@naver.com>
2023-04-22 13:34:02 +0900

    계층적 상속 메서드 호출하기

---
+++ chap08/Inheritance04.java
@@ -0,0 +1,14 @@
+package chap08;
+
+public class Inheritance04 {
+    public static void main(String[] args) {
+        
+        SubSon objSon = new SubSon();
+        objSon.printSon();
+        objSon.printFather();
+
+        SubDaughter objDaughter = new SubDaughter();
+        objDaughter.printDaughter();
+        objDaughter.printFather();
+    }
+}
